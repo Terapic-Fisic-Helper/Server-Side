30e47d7ad5e42b87a8857895b7cfdd3cb0768bdc
Hugo Arauco <araparhug7@gmail.com>
2021-05-01 21:47:31 -0500

    feature-tagSession
    feature tagSession

---
+++ TerapicFisicHelper.Entities/TagSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Entities
+{
+    public class TagSession
+    {
+        public int TagId { get; set; }
+        public int SessionId { get; set; }
+        public Tag Tag { get; set; }
+        public Session Session { get; set; }
+    }
+}
