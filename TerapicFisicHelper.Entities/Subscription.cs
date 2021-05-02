32636cfd439283ad2d84167ad49a45d9e17fc972
Andre <andrew_programador@hotmail.com>
2021-05-01 21:24:31 -0500

    feature-subscription
    feature subscription

---
+++ TerapicFisicHelper.Entities/Subscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Entities
+{
+    public class Subscription
+    {
+        public int CustomerId { get; set; }
+        public int SubscriptionPlanId { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public SubscriptionPlan SubscriptionPlan { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
