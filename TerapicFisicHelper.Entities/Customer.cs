c36d44f2fa7e77874d51c545243ddcdc400fd383
Andre <andrew_programador@hotmail.com>
2021-05-01 19:31:06 -0500

    feature-customer
    feature customer

---
+++ TerapicFisicHelper.Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
