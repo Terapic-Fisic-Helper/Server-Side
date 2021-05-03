5b7a0d0a74ce68ad54577295b4a61a1443b66e6e
Andre <andrew_programador@hotmail.com>
2021-05-03 02:13:25 -0500

    feature-startup
    feature startup

--- TerapicFisicHelper.Web/Startup.cs
+++ TerapicFisicHelper.Web/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 using TerapicFisicHelper.Data;
 
 namespace TerapicFisicHelper.Web
@@ -25,7 +26,18 @@ namespace TerapicFisicHelper.Web
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TerapicFisicHelper.Web", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "TerapicFisicHelper.Web",
+                    Version = "v1",
+                    Description = "API TerapicFisicHelper del curso Aplicaciones Web con NetCore",
+                    TermsOfService = new Uri("https://github.com/Terapic-Fisic-Helper/Server-Side/tree/develop"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Grupo EZCODE - TerapicFisicHelper",
+                        Email = "u201621283@upc.edu.pe",
+                        Url = new Uri("https://github.com/Terapic-Fisic-Helper/Server-Side),
+                    }
+                });
             });
 
             services.AddDbContext<DbContextTerapicFisicHelperApp>(options => options.UseSqlServer(Configuration.GetConnectionString("ConexionTFH")));
