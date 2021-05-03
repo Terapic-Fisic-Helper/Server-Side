be7ab59f63322f8be83c8e20c295220369bdf2af
Andre <andrew_programador@hotmail.com>
2021-05-03 06:17:17 -0500

    feature-models
    feature models

---
+++ TerapicFisicHelper.Web/Models/CreateCustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Web.Models
+{
+    public class CreateCustomerModel
+    {
+        [Required(ErrorMessage = "Todo cliente debe tener una descripción")]
+        [StringLength(255, MinimumLength = 3, ErrorMessage = "Descripcion del cliente debe tener entre 3 a 255 caracteres")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Debe incluir el codigo del usuario")]
+        [StringLength(5, MinimumLength = 3, ErrorMessage = "El codigo del usuario debe tener de 3 a 5 caracteres")]
+        public int UserId { get; set; }
+
+    }
+}
