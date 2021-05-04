3078846eeb628bfcd59c31bf006d573f8ad444c0
Andre <andrew_programador@hotmail.com>
2021-05-04 13:39:08 -0500

    feature-models
    feature models

---
+++ TerapicFisicHelper.Web/Models/SubscriptionPlanModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Web.Models
+{
+    public class SubscriptionPlanModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Cost { get; set; }
+    }
+}
---
+++ TerapicFisicHelper.Web/Models/TagSessionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Web.Models
+{
+    public class TagSessionModel
+    {
+        public int TagId { get; set; }
+        public int SessionId { get; set; }
+    }
+}
