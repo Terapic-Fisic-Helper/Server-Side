e24cb2d5067dd202671fa6536339d07985610489
Andre <andrew_programador@hotmail.com>
2021-05-03 23:08:19 -0500

    feature-models
    feature models

---
+++ TerapicFisicHelper.Web/Models/SessionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Web.Models
+{
+    public class SessionModel
+    {
+        public int Id { get; set; }
+        public int SpecialistId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public string StartHour { get; set; }
+        public string EndHour { get; set; }
+    }
+}
