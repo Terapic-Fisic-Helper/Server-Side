5562648061d75a2d0530214236212db2bbb2fe72
Andre <andrew_programador@hotmail.com>
2021-05-04 03:51:15 -0500

    feature-models
    feature models

---
+++ TerapicFisicHelper.Web/Models/HistoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TerapicFisicHelper.Web.Models
+{
+    public class HistoryModel
+    {
+        public int CustomerId { get; set; }
+        public int SessionId { get; set; }
+        public DateTime Watched { get; set; }
+    }
+}
