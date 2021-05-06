4ac504253498b2001c06a2bf5e3238db94ab06d4
TorreMarcelo <torremarcelo19@gmail.com>
2021-05-06 17:52:11 -0500

    create feature - test2

---
+++ TerapicFisicHelper.NUnitTest/SpecialistTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework; 
+using TerapicFisicHelper.Entities;
+
+namespace TerapicFisicHelper.UnitTest
+{
+    [TestFixture]
+    public class SpecialistTest
+    {
+        [Test]
+        public void SpecialtyTest(){
+            Specialist specialist = new Specialist();
+            specialist.Specialty = "especialidad1";
+            Assert.IsNotNull(specialist.Specialty);
+        }
+
+        [Test]
+        public void UserIdTest(){
+            Specialist specialist = new Specialist();
+            specialist.UserId = 1234;
+            Assert.IsNotNull(specialist.UserId);
+        }
+    }
+}
---
+++ TerapicFisicHelper.NUnitTest/TagSessionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TerapicFisicHelper.Entities;
+
+namespace TerapicFisicHelper.UnitTest
+{
+    [TestFixture]
+    public class TagSessionTest
+    {
+        [Test]
+        public void TagIdTest()
+        {
+            TagSession tagsession = new TagSession();
+            tagsession.TagId = 1234;
+            Assert.IsNotNull(tagsession.TagId);
+        }
+
+        [Test]
+        public void SessionIdTest()
+        {
+            TagSession tagsession = new TagSession();
+            tagsession.SessionId = 1;
+            Assert.IsNotNull(tagsession.SessionId);
+        }
+    }
+}
